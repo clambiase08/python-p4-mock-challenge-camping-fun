1ae44ff172465e82bdac2595c161a6044426809f
Christina Lambiase <christina.lambiase@gmail.com>
2023-08-22 10:58:24 -0700

    complete first route deliverable

--- server/app.py
+++ server/app.py
@@ -7,23 +7,28 @@ from flask import Flask, make_response, jsonify, request
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
+api = Api(app)
 
 db.init_app(app)
 
 
-@app.route('/')
-def home():
-    return ''
+class Campers(Resource):
+    def get(self):
+        campers = [c.to_dict(rules=("-signups",)) for c in Camper.query.all()]
+        return make_response(campers, 200)
 
-if __name__ == '__main__':
+
+api.add_resource(Campers, "/campers")
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
